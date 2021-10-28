cf47160fc466e56f08796eef314a0fbecb589862
Jigar <shahjigar185@gmail.com>
2021-10-28 18:11:16 +0530

    improvments and bug fixes

--- frontend/src/components/Container.js
+++ frontend/src/components/Container.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import './container.css'
 
-function Container({children,width,marginTop,padding}) {
+function Container({children,width,padding}) {
     const containerStyle={
         background: '#3F3160',
         margin:'auto',
         width:'90%',
-        padding:'30px'
-        
+        padding:'30px',
     }
     return (
         <div className="container-style">
--- frontend/src/components/Todo.js
+++ frontend/src/components/Todo.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { useHistory } from "react-router-dom";
-
+import {useEffect,useState} from 'react';
 
 function Todo({todo}) {
     const history = useHistory();
+
     const todoStyle={
         display:'flex',
         marginTop:'20px'
     }
     const textStyle={
-        backgroundColor:'#1B7089',
+        backgroundColor:todo.status=='new'?'#1B7089':todo.status=='completed'?'#4BB543':'#BF07AC',
         color:'white',
         width:'80%',
         marginRight:'10px',
@@ -20,12 +21,14 @@ function Todo({todo}) {
         backgroundColor:'#CA9C18',
         borderRadius: "10px"
     }
+
     return (
+        
         <div style={todoStyle}>
             <div style={textStyle}>
                 {todo.text}
             </div>
-            <button className="btn" style={btnStyle} onClick={()=>history.push(`/${todo._id}`)}>
+            <button className="btn" style={btnStyle} onClick={()=>history.push(`/edit/${todo._id}`)}>
                 Details
             </button>
         </div>
